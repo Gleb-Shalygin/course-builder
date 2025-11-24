d1bc6d2b7ae0aa99f0db54c9773db141b563988c
gleb-shalygin <gleb.krolik@bk.ru>
2025-11-24 22:58:40 +0600

    Добавил карточку с тестами. Верстка

--- resources/js/pages/profile/ProfilePage.vue
+++ resources/js/pages/profile/ProfilePage.vue
@@ -1,13 +1,15 @@
 <template>
     <a-tabs v-model:activeKey="activeKey">
-        <a-tab-pane key="1" tab="Тесты">Страница с тестами</a-tab-pane>
-        <!--                        <a-tab-pane key="2" tab="Курсы" force-render>Страница с курсами</a-tab-pane>-->
+        <a-tab-pane key="1" tab="Тесты">
+            <TableTests />
+        </a-tab-pane>
     </a-tabs>
 </template>
 
 <script lang="ts" setup>
 import { ref, defineOptions } from 'vue';
 import ProfileLayout from '@/layout/profile/ProfileLayout.vue';
+import TableTests from '@/components/profile/TableTests.vue';
 
 defineOptions({
     layout: ProfileLayout
