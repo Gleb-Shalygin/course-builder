39ec5aca84e27ee5a5a480ab558e7f5ee8e7f4de
Gleb-Shalygin <73645931+Gleb-Shalygin@users.noreply.github.com>
2026-02-11 23:58:19 +0600

    Аддаптив для мобильных устройств на странице тестов и самого профиля. (#2)

--- resources/js/layout/profile/ProfileHeader.vue
+++ resources/js/layout/profile/ProfileHeader.vue
@@ -1,16 +1,43 @@
 <template>
     <a-layout-header class="profile-header">
         <div class="profile-header__custom">
-            <logo />
+            <div class="profile-header__burger" @click="openMenu">
+                <MenuOutlined />
+            </div>
+
+            <Logo />
 
             <ExitButton />
         </div>
+
+        <a-drawer
+            :open="isMenuOpen"
+            placement="left"
+            :width="280"
+            :closable="false"
+            root-class-name="profile-sidebar-drawer"
+            @close="closeMenu"
+        >
+            <ProfileSidebarContent />
+        </a-drawer>
     </a-layout-header>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import { ref } from 'vue';
+import { MenuOutlined } from '@ant-design/icons-vue';
 import Logo from '@/layout/profile/Logo.vue';
 import ExitButton from '@/components/profile/ExitButton.vue';
+import ProfileSidebarContent from '@/layout/profile/ProfileSidebarContent.vue';
 
+const isMenuOpen = ref(false);
 
+const openMenu = () => {
+    isMenuOpen.value = true;
+};
+
+const closeMenu = () => {
+    isMenuOpen.value = false;
+};
 </script>
+
