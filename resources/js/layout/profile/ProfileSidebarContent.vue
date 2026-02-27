f384a8d7f7d3f833b106ffcf2c027cccd918ef80
gleb-shalygin <gleb.krolik@bk.ru>
2026-02-27 16:49:49 +0600

    Перенос профиля на отдельную страницу

--- resources/js/layout/profile/ProfileSidebarContent.vue
+++ resources/js/layout/profile/ProfileSidebarContent.vue
@@ -4,10 +4,24 @@
 
         <ProfileUserCard />
 
-        <!--
-          Навигационные ссылки профиля должны располагаться здесь.
-          Этот же компонент переиспользуется внутри бургер-меню.
-        -->
+        <div class="profile-sidebar__nav">
+            <router-link
+                to="/profile"
+                class="profile-sidebar__nav-link"
+                active-class="profile-sidebar__nav-link--active"
+                exact
+            >
+                Профиль
+            </router-link>
+            <router-link
+                to="/profile/tests"
+                class="profile-sidebar__nav-link"
+                active-class="profile-sidebar__nav-link--active"
+            >
+                Тесты
+            </router-link>
+        </div>
+
         <slot />
     </div>
 </template>
