bd48267bee150e5d0155c185689db491c5c80355
gleb-shalygin <gleb.krolik@bk.ru>
2026-03-12 17:25:59 +0600

    Обновил api

--- routes/api.php
+++ routes/api.php
@@ -5,12 +5,14 @@ use App\Http\Controllers\Profile\TestController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('web')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('logout', [AuthController::class, 'logout']);
-    Route::get('user', [AuthController::class, 'user']);
-    Route::get('tests', [TestController::class, 'tests']);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('logout', [AuthController::class, 'logout']);
+        Route::get('user', [AuthController::class, 'user']);
+        Route::get('tests', [TestController::class, 'tests']);
+    });
 });
 
