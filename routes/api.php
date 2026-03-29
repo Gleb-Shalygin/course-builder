1b9b021234f266dc3bf0c0eb4d80809643fd6712
gleb-shalygin <gleb.krolik@bk.ru>
2026-03-29 19:27:32 +0600

    Простой тест на получение тестов, без дополнительных кейсов

--- routes/api.php
+++ routes/api.php
@@ -5,14 +5,14 @@ use App\Http\Controllers\Profile\TestController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::middleware('web')->group(function () {
+Route::middleware('web')->name('web.')->group(function () {
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
 
     Route::middleware('auth:sanctum')->group(function () {
         Route::post('logout', [AuthController::class, 'logout']);
         Route::get('user', [AuthController::class, 'user']);
-        Route::get('tests', [TestController::class, 'tests']);
+        Route::get('tests', [TestController::class, 'tests'])->name('tests');
     });
 });
 
--- tests/Feature/ProfileTestListTest.php
+++ tests/Feature/ProfileTestListTest.php
@@ -2,6 +2,9 @@
 
 namespace Tests\Feature;
 
+use App\Models\Test\Test;
+use Database\Seeders\TestSeeder;
+use Illuminate\Support\Facades\Auth;
 use Tests\TestCase;
 
 class ProfileTestListTest extends TestCase
@@ -11,8 +14,30 @@ class ProfileTestListTest extends TestCase
      */
     public function test_list(): void
     {
-        $response = $this->get('/api/tests');
+        $this->seed(TestSeeder::class);
 
+        $user = Auth::user();
+
+        $tests = Test::query()->where('user_id', $user->id)
+            ->withCount(['testAttempt as count_finished' => function ($query) {
+                $query->whereNotNull('finished_at');
+            }])->get(['id', 'title', 'description', 'is_public']);
+
+        $testFakeResponse = ['data' => []];
+
+        foreach ($tests as $test) {
+            $testFakeResponse['data'][] = [
+                'id' => $test->id,
+                'title' => $test->title,
+                'description' => $test->description,
+                'is_public' => $test->is_public,
+                'count_finished' => $test->count_finished
+            ];
+        }
+
+        $response = $this->getJson(route('web.tests'));
+
+        $response->assertExactJson($testFakeResponse);
         $response->assertStatus(200);
     }
 }
