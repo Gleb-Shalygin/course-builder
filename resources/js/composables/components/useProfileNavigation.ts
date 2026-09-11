d13991e4a25cf72c8d144ef75763e7b0e23614b2
gleb-shalygin <gleb.krolik@bk.ru>
2026-09-11 21:37:02 +0600

    Мелочные косяки в дизайне

--- resources/js/composables/components/useProfileNavigation.ts
+++ resources/js/composables/components/useProfileNavigation.ts
@@ -1,12 +1,11 @@
 import { computed } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
-import { AppstoreOutlined, PlusCircleOutlined, UserOutlined } from '@ant-design/icons-vue';
+import { AppstoreOutlined, UserOutlined } from '@ant-design/icons-vue';
 import type { ProfileMenuItem } from '@/types/ProfileMenuItem';
 
 const items: ProfileMenuItem[] = [
     { key: 'profile', label: 'Профиль', icon: UserOutlined },
     { key: 'profile-tests', label: 'Тесты', icon: AppstoreOutlined },
-    { key: 'test-create', label: 'Создать тест', icon: PlusCircleOutlined },
 ];
 
 export function useProfileNavigation() {
