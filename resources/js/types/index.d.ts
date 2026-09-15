8d9f9529de4e3666088c661ca280240f22412063
gleb-shalygin <gleb.krolik@bk.ru>
2026-09-15 21:06:51 +0600

    Перенес хранение svg в отдельную папку, а само отображение сделал через один компонент.

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -2,12 +2,6 @@ export interface Auth {
     user: User;
 }
 
-export type AppPageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
-    name: string;
-    quote: { message: string; author: string };
-    auth: Auth;
-};
-
 export interface User {
     id: number;
     name: string;
